2814433074033f5678cf6ac586cd2836f3ce735b
kouassives <kouassives@gmail.com>
2018-01-18 17:13:00 +0000

    Sortir du programme si la recuperation du registre echoue

--- RMI_ClientServer_Client/src/main/Client.java
+++ RMI_ClientServer_Client/src/main/Client.java
@@ -24,6 +24,7 @@ public class Client {
 	} catch (RemoteException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
+		System.exit(-1);
 	}
 		
     boolean rechercherCompte = true;
